4636b4effb7fe6ea00c0a46111a741183698e488
Sohel Dhengre <soheldhengre@gmail.com>
2018-01-30 11:42:28 +0530

    first commit

---
+++ Job_Seeker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Job_Seeker
+//
+//  Created by Sohel Dhengre on 30/01/18.
+//  Copyright © 2018 Sohel Dengre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
