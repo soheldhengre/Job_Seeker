f6e22328703712099bf8560abc757f01abbc8590
RD <rajudhumne@gmail.com>
2018-02-01 15:06:26 +0530

    added tab bar

---
+++ Job_Seeker/Controller/JobSeekerSetting.swift
@@ -0,0 +1,21 @@
+//
+//  JobSeekerSetting.swift
+//  Job_Seeker
+//
+//  Created by Raju Dhumne on 01/02/18.
+//  Copyright © 2018 Sohel Dengre. All rights reserved.
+//
+
+import UIKit
+
+class JobSeekerSetting: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
