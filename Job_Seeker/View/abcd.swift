f592af5eec1388c3ea5f4b6faddecd5710114b26
RD <rajudhumne@gmail.com>
2018-02-02 22:36:11 +0530

    ui changes

---
+++ Job_Seeker/View/abcd.swift
@@ -0,0 +1,24 @@
+//
+//  abcd.swift
+//  Job_Seeker
+//
+//  Created by Raju Dhumne on 02/02/18.
+//  Copyright © 2018 Sohel Dengre. All rights reserved.
+//
+
+import UIKit
+
+class abcd: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
