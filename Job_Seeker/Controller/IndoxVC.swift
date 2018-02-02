369ca6ac8c67d70fa8a81dabc01c6426e0e517b6
RD <rajudhumne@gmail.com>
2018-02-02 11:52:31 +0530

    done with tabbar

---
+++ Job_Seeker/Controller/IndoxVC.swift
@@ -0,0 +1,21 @@
+//
+//  IndoxVC.swift
+//  Job_Seeker
+//
+//  Created by Raju Dhumne on 02/02/18.
+//  Copyright © 2018 Sohel Dengre. All rights reserved.
+//
+
+import UIKit
+
+class IndoxVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
---
+++ Job_Seeker/Controller/JobSeekerInboxVC.swift
@@ -0,0 +1,25 @@
+//
+//  JobSeekerInboxVC.swift
+//  Job_Seeker
+//
+//  Created by Raju Dhumne on 02/02/18.
+//  Copyright © 2018 Sohel Dengre. All rights reserved.
+//
+
+import UIKit
+
+class JobSeekerInboxVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+
+    
+    }
+
+ 
+}
