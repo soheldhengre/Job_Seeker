c3f2903bb35aaa0c6777ecc9909e141fb3da3ac4
RD <rajudhumne@gmail.com>
2018-02-07 14:29:39 +0530

    personalDetailsVC

---
+++ Job_Seeker/Controller/JobSeekerProfileDetailsVC.swift
@@ -0,0 +1,53 @@
+//
+//  JobSeekerProfileDetailsVC.swift
+//  Job_Seeker
+//
+//  Created by Raju Dhumne on 07/02/18.
+//  Copyright © 2018 Sohel Dengre. All rights reserved.
+//
+
+import UIKit
+
+class JobSeekerProfileDetailsVC: UIViewController {
+
+    @IBOutlet var availableType: [UIButton]!
+    @IBOutlet var jobType: [UIButton]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    @IBAction func handelTappedSelection(_ sender: UIButton) {
+        jobType.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+        
+        
+    }
+    @IBAction func immediatelyAvailableHandelTapped(_ sender: UIButton) {
+        availableType.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+            
+        }
+    }
+    @IBAction func jobTypeTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func availableTypeTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+    
+    
+}
