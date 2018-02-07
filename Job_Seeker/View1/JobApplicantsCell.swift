8819e0f32057fd6cb1c87e2e65ce4bb4e91cfe9a
Sohel Dhengre <soheldhengre@gmail.com>
2018-02-07 14:31:41 +0530

    ui fixes

---
+++ Job_Seeker/View1/JobApplicantsCell.swift
@@ -0,0 +1,24 @@
+//
+//  JobApplicantsCell.swift
+//  Job_Seeker
+//
+//  Created by Sohel Dhengre on 07/02/18.
+//  Copyright © 2018 Sohel Dengre. All rights reserved.
+//
+
+import UIKit
+
+class JobApplicantsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
